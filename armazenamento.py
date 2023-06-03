22baf02b5088b3e1c93268c6e54952acd695c136
raularaju <raularaju@uol.com.br>
2023-06-03 19:03:41 -0300

    ref: pequena refatoração

--- armazenamento.py
+++ armazenamento.py
@@ -5,9 +5,9 @@ def armazena_grafos_visibilidade_e_features(arquivo_saida, id_exame, matriz_feat
     grupo_exame = arquivo_saida.create_group('exame {}'.format(id_exame))
     for j in range(8):
         grupo_lead = grupo_exame.create_group('lead {}'.format(j))
+        grupo_lead.create_dataset('features', data=matriz_features[j])
         grupo_grafo = grupo_lead.create_group('grafo')
         grupo_grafo.create_dataset('data', data=grafos_visibilidade[j].data)
         grupo_grafo.create_dataset('row', data=grafos_visibilidade[j].row)
         grupo_grafo.create_dataset('col', data=grafos_visibilidade[j].col)
-        grupo_lead.create_dataset('features', data=matriz_features[j])
     print("Dados armazenados com sucesso")

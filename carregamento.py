222ac6956a8d4554afac5e5f3b38ad452400cf8b
raularaju <raularaju@uol.com.br>
2023-09-11 19:35:03 -0300

    ref: pequenas mudanças

--- carregamento.py
+++ carregamento.py
@@ -6,7 +6,6 @@ def carrega_series(caminho_arquivo: str, inicio: int, fim: int):
 
     with h5py.File(caminho_arquivo,'r') as arquivo:
         print(f"Carregando as séries de {inicio} até {fim}")
-
         traces_ids = np.array(arquivo['exam_id'][inicio:fim])
         tracings = np.array(arquivo['tracings'][inicio:fim])
         print("Séries carregada com tamanho {}".format(fim-inicio))   
